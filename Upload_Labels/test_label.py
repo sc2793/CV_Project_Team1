75776ba9b547099225414839b9906410d0173d46
Rachana Chaudhari <rachanachaudhari16@gmail.com>
2023-04-22 02:21:59 -0400

    Structured code a little

--- Upload_Labels/test_label.py
+++ Upload_Labels/test_label.py
@@ -4,9 +4,41 @@ sample_label = {
     "segments" : [{
         "keyframes" : [
             {
+              "frame": 10,
+              "bbox" : {
+                "top": 50.0,
+                "left": 98.0,
+                "height": 382.0,
+                "width": 341.0
+              } ,
+             "classifications" : [
+              {
+              "name": "Type",
+              "answer": {"name": "Van"}
+            }
+            ]
+             
+           },
+           {
+              "frame": 11,
+              "bbox" : {
+                "top": 30.0,
+                "left": 98.0,
+                "height": 382.0,
+                "width": 341.0
+              } ,
+             "classifications" : [
+              {
+              "name": "Type",
+              "answer": {"name": "Van"}
+            }
+            ]
+             
+           },
+           {
               "frame": 13,
               "bbox" : {
-                "top": 146.0,
+                "top": 45.0,
                 "left": 98.0,
                 "height": 382.0,
                 "width": 341.0
--- Upload_Labels/upload_labels.py
+++ Upload_Labels/upload_labels.py
@@ -1,61 +1,55 @@
 import labelbox as lb
-import labelbox.types as lb_types
 import uuid
 from test_label import sample_label
 
 class UploadLabel(object):
-    def __init__(self):
+    def __init__(self, labels, project_id, ontology_id=""):
         print("initialised")
-        API_KEY=""
+        API_KEY=input("Enter API Key")
         self.client = lb.Client(api_key=API_KEY)
-        # self.ontology = client.get_ontology("clevph27n2o060723bkfw16r4")
-        self.global_key = ""
-        self.project = self.client.get_project("")
+        # self.ontology = client.get_ontology(ontology_id)
+        self.project = self.client.get_project(project_id)
+        self.labels = labels if isinstance(labels, list) else [labels]
         # self.project.setup_editor(self.ontology)
 
-    def upload_video(self):
+    def upload_video(self, global_key, row_data, dataset="video_demo_dataset"):
         client = self.client
-        self.global_key = "2118--41.mp4"
         asset = {
-            "row_data": "/Users/rachanachaudhari/Projects/CV-project-Team-2/UploadLabels/2118--41.mp4", 
-            "global_key": self.global_key,
+            "row_data": row_data, 
+            "global_key": global_key,
             "media_type": "VIDEO"
         }
-
-        dataset = client.create_dataset(name="video_demo_dataset")
+        dataset = client.create_dataset(name=dataset)
         task = dataset.create_data_rows([asset])
         task.wait_till_done()
         print("Errors :",task.errors)
         print("Failed data rows:", task.failed_data_rows)   
 
-    def upload_batch_videos(self):
+    def upload_batch_videos(self, global_keys):
         project = self.project
-        # # Create a batch to send to your MAL project
         batch = project.create_batch(
-        "first-batch-video-demo1", # Each batch in a project must have a unique name
-        global_keys=[self.global_key], # A paginated collection of data row objects, a list of data rows or global keys
-        priority=5 # priority between 1(Highest) - 5(lowest)
+            "first-batch-video-demo1", # Each batch in a project must have a unique name
+            global_keys=global_keys, # A paginated collection of data row objects, a list of data rows or global keys
+            priority=5 # priority between 1(Highest) - 5(lowest)
         )
-
         print("Batch: ", batch)
 
-    def upload_label(self):
+    def upload_labels(self):
         upload_job_label_import = lb.LabelImport.create_from_objects(
             client = self.client,
             project_id = self.project.uid, 
             name = "label_import_job-" + str(uuid.uuid4()),
-            labels=[sample_label]
-            )
-
+            labels=self.labels
+        )
         upload_job_label_import.wait_until_done()
         print("Errors:", upload_job_label_import.errors)
         print("Status of uploads: ", upload_job_label_import.statuses)
-        print("   ")
+
             
     def run(self):
-        # self.upload_video()
-        self.upload_label()
+        # self.upload_video(global_key="5023--41.mp4", row_data="/Users/rachanachaudhari/Projects/CV-project-Team-2/Test_Videos/5023--41.mp4")
+        self.upload_labels()
 
 if __name__ == '__main__':
-    u = UploadLabel()
+    u = UploadLabel(sample_label, project_id="clgl0y7dg0cxi07xj3b7k6gy4")
     u.run()
